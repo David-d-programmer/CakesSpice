a9220cef8b1adef20d18a4dcf6ab467d735f6719
David Okon <davidokon769@gmail.com>
2024-10-17 01:19:06 +0000

    added home app and templates

--- proj5/settings.py
+++ proj5/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
 ]
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'proj5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
