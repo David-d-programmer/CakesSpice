edd2150be57f8622f2d3f94fa23ee46192d13d5b
David Okon <davidokon769@gmail.com>
2025-01-03 18:13:50 +0000

    making right the categories.json file

--- products/models.py
+++ products/models.py
@@ -1,25 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
+    name = models.CharField(max_length=100)
+    description = models.TextField(default=now)
+    created_at = models.DateTimeField(default=now)
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
