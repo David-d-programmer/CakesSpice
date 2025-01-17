a288df58fec60afe71be8ca2315fae7357ee1731
David Okon <davidokon769@gmail.com>
2025-01-17 17:13:50 +0000

    changes done and ready to deploy

--- proj5/settings.py
+++ proj5/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-gt@$vqpg!z*9d61$#lcgc-)y7utlz*ug&%aop@ylymq25w%aw^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-daviddprogra-cakesspice-efmtc4x7qtp.ws-eu117.gitpod.io']
+ALLOWED_HOSTS = ['cakesspice', 'local host']
 
 
 
