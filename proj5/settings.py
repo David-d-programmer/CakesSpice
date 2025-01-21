4d91f1933096ae122877c0644ba81444ca72f5c1
David Okon <davidokon769@gmail.com>
2025-01-21 16:09:32 +0000

    updated settings

--- proj5/settings.py
+++ proj5/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '3b_daxvhwj_hiftfe#+3oj8sdhc%(+ne#-5e((1wehzm2%r-wz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['cakesspice-d57190d7147c.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-daviddprogra-cakesspice-efmtc4x7qtp.ws-eu117.gitpod.io', 'localhost']
 
 
 
