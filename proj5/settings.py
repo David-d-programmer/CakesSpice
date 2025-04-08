c16a30d4b607cbc63a5cc7cdbbb72254e4891fc8
David Okon <davidokon769@gmail.com>
2025-04-08 07:50:19 +0000

    settings updated

--- proj5/settings.py
+++ proj5/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '3b_daxvhwj_hiftfe#+3oj8sdhc%(+ne#-5e((1wehzm2%r-wz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['cakesspice-d57190d7147c.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-daviddprogra-cakesspice-1odgbs0dfx0.ws-eu118.gitpod.io', 'localhost']
 
 
 
@@ -194,7 +194,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'cakesspices' # change this to your AWS bucket name
     AWS_S3_REGION_NAME = 'us-east-1'
@@ -219,5 +219,17 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
-DEFAULT_FROM_EMAIL = 'davidokon646@yahoo.com'
-print("Stripe Secret Key:", STRIPE_SECRET_KEY)
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'davidokon646@yahoo.com'
+
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
